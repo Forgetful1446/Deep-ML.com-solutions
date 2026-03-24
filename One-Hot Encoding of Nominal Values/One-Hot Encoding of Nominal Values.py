90455b7a342e01cc11006c011fca5c90bd84e2bb
Ngô Hoàng Khải <nkhai1446@gmail.com>
2026-03-24 18:18:03 +0700

    Refactor to_categorical function for clarity and safety

--- One-Hot Encoding of Nominal Values/One-Hot Encoding of Nominal Values.py
+++ One-Hot Encoding of Nominal Values/One-Hot Encoding of Nominal Values.py
@@ -1,11 +1,13 @@
-import numpy as np
-
 def to_categorical(x, n_col=None):
-	if n_col is not None:
-		result = [[0. for _ in range(n_col)] for _ in range(len(x))]
-	else:
-		result = [[0. for _ in range(max(x) + 1)] for _ in range(len(x))]
+	if n_col is None:
+		n_col = max(x) + 1
+	
+	if max(x) >= n_col:
+		raise ValueError("n_col must be greater than max value in x")
+	
+	result = [[0. for _ in range(n_col)] for _ in range(len(x))]
 	
 	for i in range(len(x)):
 		result[i][x[i]] = 1.0
+	
 	return result
